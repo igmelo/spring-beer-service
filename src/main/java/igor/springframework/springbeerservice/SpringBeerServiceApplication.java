542c724b519bdb6b79864faa4fea3967324f6d5a
Igor Melo <igorgalvao@gec.inatel.br>
2021-02-17 13:57:41 -0300

    Initial Project

---
+++ src/main/java/igor/springframework/springbeerservice/SpringBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package igor.springframework.springbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/igor/springframework/springbeerservice/SpringBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package igor.springframework.springbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
