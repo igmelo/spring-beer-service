cd9ed8b0f06a5a8677e75d8f4ed4a5a09e36ab8e
Igor Melo <igorgalvao@gec.inatel.br>
2021-02-18 11:40:14 -0300

    Adding error handler - closes #8

--- src/main/java/igor/springframework/springbeerservice/web/controller/BeerController.java
+++ src/main/java/igor/springframework/springbeerservice/web/controller/BeerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -18,13 +19,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDTO beerDTO){
+    public ResponseEntity saveNewBeer(@Valid @RequestBody BeerDTO beerDTO){
         //todo impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable UUID id, @RequestBody BeerDTO beerDTO){
+    public ResponseEntity updateBeerById(@PathVariable UUID id,@Valid @RequestBody BeerDTO beerDTO){
         //todo impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
