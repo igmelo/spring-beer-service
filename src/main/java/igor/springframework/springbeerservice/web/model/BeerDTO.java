d2df4b11e1b109443f229b58f61e80e76707c3d4
Igor Melo <igorgalvao@gec.inatel.br>
2021-02-19 13:35:27 -0300

    Adding Jackson Config for BeerDTO - closes #12

--- src/main/java/igor/springframework/springbeerservice/web/model/BeerDTO.java
+++ src/main/java/igor/springframework/springbeerservice/web/model/BeerDTO.java
@@ -30,11 +30,11 @@ public class BeerDTO {
     private Integer version;
 
     @Null
-    @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
 
     @Null
-    @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedData;
 
     @NotBlank
