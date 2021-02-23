84f14a3b263d10288ed82bc14f3062b6fcca80e7
Igor Melo <igorgalvao@gec.inatel.br>
2021-02-23 13:44:02 -0300

    Adding ehcache for list beer and get beer - closes #19

---
+++ src/main/java/igor/springframework/springbeerservice/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package igor.springframework.springbeerservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
