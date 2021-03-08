dcd51517d41d43d9ce8c425c1db4fa3f8f135979
Igor Melo <igorgalvao@gec.inatel.br>
2021-03-08 11:13:26 -0300

    Adding Validate Order Listener - closes #31

---
+++ src/main/java/igor/springframework/brewery/model/events/ValidateOrderRequest.java
@@ -0,0 +1,16 @@
+package igor.springframework.brewery.model.events;
+
+import igor.springframework.brewery.model.BeerOrderDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ValidateOrderRequest {
+
+    private BeerOrderDto beerOrder;
+}
